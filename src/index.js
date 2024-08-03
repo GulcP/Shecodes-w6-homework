6af4f7aa5a4af57e4efcc70e0c00cc5434b0bd3f
GulcP <gulcinparmentier@gmail.com>
2024-08-03 19:55:01 +0200

    Update index.js
    Updated with Weather API

--- src/index.js
+++ src/index.js
@@ -1,53 +1,49 @@
-function displayTemperature(response) {
-	let temperatureElement = document.querySelector("#current-temperature");
-	let temperature = Math.round(response.data.temperature.current);
-	let cityElement = document.querySelector("#current-city");
-	cityElement.innerHTML = response.data.city;
-	temperatureElement.innerHTML = temperature;
+function displayCurrentTemp(response) {
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  let currentTemperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = currentTemperature;
 }
 
 function search(event) {
-	event.preventDefault();
-	let searchInputElement = document.querySelector("#search-input");
-	let city = searchInputElement.value;
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let city = searchInput.value.trim().toLowerCase();
 
-	let apiKey = "b2a5adcct04b33178913oc335f405433";
-	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=ca4bof60fdf24464tc7e37793a25dcc2&units=metric`;
 
-	axios.get(apiUrl).then(displayTemperature);
-}
-
-function formatDate(date) {
-	let minutes = date.getMinutes();
-	let hours = date.getHours();
-	let day = date.getDay();
-
-	if (minutes < 10) {
-		minutes = `0${minutes}`;
-	}
-
-	if (hours < 10) {
-		hours = `0${hours}`;
-	}
-
-	let days = [
-		"Sunday",
-		"Monday",
-		"Tuesday",
-		"Wednesday",
-		"Thursday",
-		"Friday",
-		"Saturday",
-	];
-
-	let formattedDay = days[day];
-	return `${formattedDay} ${hours}:${minutes}`;
+  axios.get(apiUrl).then(displayCurrentTemp);
 }
 
 let searchForm = document.querySelector("#search-form");
+console.log(searchForm);
 searchForm.addEventListener("submit", search);
 
-let currentDateELement = document.querySelector("#current-date");
-let currentDate = new Date();
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = date.getDay();
 
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let formatDay = days[day];
+  return `${formatDay} ${hours}:${minutes}`;
+}
+let currentDate = new Date();
+let currentDateELement = document.querySelector(".current-date");
 currentDateELement.innerHTML = formatDate(currentDate);
